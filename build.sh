2f885eb19fcccfd208a289a638c3e6bf444a6baf
Julio Cezar Riffel <julioriffel@gmail.com>
2024-02-26 22:21:31 -0300

    v1.1.1 Unlogged Table

---
+++ build.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (c) 2024.
+# Julio Cezar Riffel
+# https://www.linkedin.com/in/julio-cezar-riffel/
+# https://github.com/julioriffel
+#
+#
+
+docker build -t julioriffel/rinha_backend_django:latest .
+docker pull julioriffel/rinha_backend_django:latest
